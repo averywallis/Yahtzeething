5e27dcfb2618f2d923afdecffb696ab0aa06cb30
averywallis <averybwallis@gmail.com>
2016-04-19 14:39:58 -0400

    Updated from Brython Server: 4/19/2016 2:39:57 PM

--- FinalProject.py
+++ FinalProject.py
@@ -49,7 +49,7 @@ class Chell(Sprite):
         self.mleft = 0
         self.alt = 0
         self.image = 0
-        self.scale = .5
+        self.scale = .25
         PortalGame.listenKeyEvent("keydown", "d", self.rightOn)
         PortalGame.listenKeyEvent("keyup", "d", self.rightOff)
         PortalGame.listenKeyEvent("keydown", "a", self.leftOn)
