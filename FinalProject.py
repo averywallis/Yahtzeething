6ff0a152f19efb53bfca504c3430a64b7873a161
averywallis <averybwallis@gmail.com>
2016-04-22 14:41:46 -0400

    Updated from Brython Server: 4/22/2016 2:41:46 PM

--- FinalProject.py
+++ FinalProject.py
@@ -43,7 +43,7 @@ class Chell(Sprite):
     asset = ImageAsset("images/Chell1.png")
     assetflip = ImageAsset("images/Chell2.png")
     def __init__(self, position):
-        super().__init__(Chell.asset,Chelll.assetfril position)
+        super().__init__(Chell.asset,Chelll.assetflip, position)
         self.visible = True
         self.x = 250
         self.y = 250
