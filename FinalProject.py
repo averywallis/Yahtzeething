abf563635d9f1462636d931f66323bfa712a28a5
averywallis <averybwallis@gmail.com>
2016-04-25 14:14:45 -0400

    Updated from Brython Server: 4/25/2016 2:14:44 PM

--- FinalProject.py
+++ FinalProject.py
@@ -40,7 +40,7 @@ doorline=LineAsset(0, 120, thinline)
 goo=PolygonAsset([(0,500),(800,500),(800,600,),(0,600)],noline,gooy)
 
 class Chell(Sprite):
-    asset = ImageAsset("images/Chell.png", Frame(100,100,50,50), 1, 'horizontal')
+    asset = ImageAsset("images/Chell.png")
     def __init__(self, position):
         super().__init__(Chell.asset, position)
         self.visible = True
