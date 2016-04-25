d68021edda97bceb70302c51ca318f65676e8c88
averywallis <averybwallis@gmail.com>
2016-04-25 14:32:58 -0400

    Updated from Brython Server: 4/25/2016 2:32:57 PM

--- FinalProject.py
+++ FinalProject.py
@@ -102,7 +102,7 @@ class Chell(Sprite):
         col = self.collidingWithSprites(OrangePortal)
         if col:
             if self.bportal:
-                self.x = self.cox-50
+                self.x = self.BluePortal.self.x
                 self.y = self.coy-50
         """        
         col1 = self.collidingWithSprites(BluePortal)
