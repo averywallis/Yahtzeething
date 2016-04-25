1c9d4a76c93070973aa0eca25d4e1524510b7b2b
averywallis <averybwallis@gmail.com>
2016-04-25 14:20:22 -0400

    Updated from Brython Server: 4/25/2016 2:20:22 PM

--- FinalProject.py
+++ FinalProject.py
@@ -102,8 +102,8 @@ class Chell(Sprite):
         col = self.collidingWithSprites(OrangePortal)
         if col:
             if self.bportal:
-                self.x = 500
-                self.y = 500
+                self.x = BluePortal
+                self.y = BluePortal
 
     def ClickOn(self,event):
         self.click = 1
