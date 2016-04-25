6fcadf1fc3bcf43a06bc968141bf23cf085d6dee
averywallis <averybwallis@gmail.com>
2016-04-25 14:48:01 -0400

    Updated from Brython Server: 4/25/2016 2:48:01 PM

--- FinalProject.py
+++ FinalProject.py
@@ -91,14 +91,14 @@ class Chell(Sprite):
                 self.click = 0
     
         if self.mright == 1:
-            self.setImage(1)
+            self.setImage(0)
             self.x += .75
             self.y = self.y
         if self.mright == 0:
             self.x =self.x
             self.y=self.y
         if self.mleft == 1:
-            self.setImage(0)
+            self.setImage(1)
             self.x += -.75
             self.y = self.y
         if self.mleft == 0:
