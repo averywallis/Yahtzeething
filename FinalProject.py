3f37520c865a62075576a6983a1392fc10b715d2
averywallis <averybwallis@gmail.com>
2016-04-26 14:37:16 -0400

    Updated from Brython Server: 4/26/2016 2:37:16 PM

--- FinalProject.py
+++ FinalProject.py
@@ -103,7 +103,7 @@ class Chell(Sprite):
             if self.bportal:
                 self.x = self.cbx
                 self.y = self.cby
-                self.setImage(0)
+                Chell((self.cbx,self.cby))
 
         
         if self.mright == 1:
