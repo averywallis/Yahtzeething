10e2eee7e20da2d8f09f4b6f68101ac829ae83ed
averywallis <averybwallis@gmail.com>
2016-04-26 14:43:29 -0400

    Updated from Brython Server: 4/26/2016 2:43:29 PM

--- FinalProject.py
+++ FinalProject.py
@@ -97,7 +97,7 @@ class Chell(Sprite):
                 self.cby = self.cpy
                 self.click = 0
         
-        if self.x<self.cox+10 and self.x>self.cox-10:
+        if self.x<=self.cox+10 and self.x>=self.cox-10:
             self.x = self.cbx
             self.y = self.cby
         """
