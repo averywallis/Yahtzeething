f84e42e2aa9c5099c91fe7c9852f976f207bff17
averywallis <averybwallis@gmail.com>
2016-05-02 14:16:35 -0400

    Updated from Brython Server: 5/2/2016 2:16:34 PM

--- FinalProject.py
+++ FinalProject.py
@@ -156,7 +156,7 @@ class PortalGame(App):
         txt_asset= TextAsset(text="WASD to Move", width=200, align='center',style='10px Arial', fill=Color(0xff2222,1))
         txt_asset1= TextAsset(text="Left Click to Place Orange Portal", width=200, align='center',style='10px Arial', fill=Color(0xff2222,1))
         txt_asset2= TextAsset(text="Alt + Left Click to Place Blue Portal", width=200, align='center',style='10px Arial', fill=Color(0xff2222,1))
-        Sprite(wall, (400,20))
+        Sprite(wall, (0,0))
         Sprite(wall, (100,20))
         Sprite(exit, (800,100))
         Sprite(exit2, (800, 100))
