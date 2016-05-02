b90d2a12d5b897ace0504aa0fc9f0e5cca709170
averywallis <averybwallis@gmail.com>
2016-05-02 14:14:36 -0400

    Updated from Brython Server: 5/2/2016 2:14:36 PM

--- FinalProject.py
+++ FinalProject.py
@@ -165,7 +165,7 @@ class PortalGame(App):
         Sprite(plat, (650, 150))
         Sprite(goo, (100,0))
         Sprite(txt_asset,(0,0))
-        Sprite(txt_asset1, (0,10))
+        Sprite(txt_asset1, (0,20))
         BluePortal((0,0))
         OrangePortal((0,0))
         Chell((0,0))
