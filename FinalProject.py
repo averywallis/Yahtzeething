961e3b9dec45490a1a274cf60ca00925fbda499f
averywallis <averybwallis@gmail.com>
2016-05-02 14:49:06 -0400

    Updated from Brython Server: 5/2/2016 2:49:06 PM

--- FinalProject.py
+++ FinalProject.py
@@ -105,7 +105,7 @@ class Chell(Sprite):
             self.x = self.x
             self.y = self.y
             
-        col = self.colliding(goo)
+        col = self.collidingWith(goo)
         if col:
             self.x = 0
             self.y = 0
