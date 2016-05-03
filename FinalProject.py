7da91c69de82ba9d9cabf772b03da22564dd73ba
averywallis <averybwallis@gmail.com>
2016-05-03 14:08:55 -0400

    Updated from Brython Server: 5/3/2016 2:08:55 PM

--- FinalProject.py
+++ FinalProject.py
@@ -105,7 +105,7 @@ class Chell(Sprite):
             self.x = self.x
             self.y = self.y
             
-        if self.y>=700:
+        if self.y>=800:
             self.x = 100
             self.y = 410
 
