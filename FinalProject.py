6f5186996f247ce269d315dad11026667f6e8890
averywallis <averybwallis@gmail.com>
2016-05-03 14:24:01 -0400

    Updated from Brython Server: 5/3/2016 2:24:01 PM

--- FinalProject.py
+++ FinalProject.py
@@ -110,8 +110,8 @@ class Chell(Sprite):
             self.y = 410
         if self.x <0:
             self.x = 0
-        if self.x > 975:
-            self.x = 975
+        if self.x > 950:
+            self.x = 950
 
 
     def ClickOn(self,event):
