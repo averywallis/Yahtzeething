081dd04ae162773c7669af3d14cde66d374286f6
averywallis <averybwallis@gmail.com>
2016-05-05 14:21:53 -0400

    Updated from Brython Server: 5/5/2016 2:21:53 PM

--- FinalProject.py
+++ FinalProject.py
@@ -129,7 +129,7 @@ class Chell(Sprite):
             self.vy = 0
             # jump
             if self.jump == 1:
-                self.vy -= 1
+                self.vy -= 5
                 self.vy = -100
                 self.jump = 0
         
