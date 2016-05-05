032016c6cbe9cdfb9b8f5b0497a7a539c9a1685c
averywallis <averybwallis@gmail.com>
2016-05-05 14:53:59 -0400

    Updated from Brython Server: 5/5/2016 2:53:58 PM

--- FinalProject.py
+++ FinalProject.py
@@ -269,7 +269,7 @@ class PortalGame(App):
         OrangePortal((0,0))
         Chell((0,0))
         # win text
-        YouWin((-100,-100))
+        YouWin((-1000,-1000))
         
     def step(self):
         for chell in self.getSpritesbyClass(Chell):
