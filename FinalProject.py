084d685a3385f3c63fbece08873ba36de05b4ad0
averywallis <averybwallis@gmail.com>
2016-05-06 14:53:58 -0400

    Updated from Brython Server: 5/6/2016 2:53:58 PM

--- FinalProject.py
+++ FinalProject.py
@@ -129,7 +129,7 @@ class Chell(Sprite):
             self.y = self.y
         # if collliding with platform, go up to counteract the moving down, effectively staying still    
         if self.collidingWithSprites(Platforms):
-            self.vy -= 1
+            self.vy -= .1
             self.y += self.vy
             self.vy = 0
             # jump
@@ -137,7 +137,7 @@ class Chell(Sprite):
                 self.vy = -3
         # constantly moving down for gravity
         else:    
-            self.vy += 1
+            self.vy += .1
             self.y += self.vy
             self.jumpon = 0
 
