4920bd288e87c05d11246d3b1b219dbef4f0c2c3
averywallis <averybwallis@gmail.com>
2016-05-06 14:07:16 -0400

    Updated from Brython Server: 5/6/2016 2:07:15 PM

--- FinalProject.py
+++ FinalProject.py
@@ -139,6 +139,7 @@ class Chell(Sprite):
                 for x in range(0,100):
                     self.vy -= 1
                     self.y += sefl.vy
+                    x += 1
         
         # death by goo    
         if self.y >= 560:
