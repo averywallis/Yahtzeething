48a2c6dde06e87a00ee56ebb9a13e296b389d19a
averywallis <averybwallis@gmail.com>
2016-05-09 14:48:13 -0400

    Updated from Brython Server: 5/9/2016 2:48:13 PM

--- FinalProject.py
+++ FinalProject.py
@@ -237,7 +237,7 @@ class CompanionCube(Sprite):
     cc = ImageAsset("images/companioncube.png")
     def __init__(self, position):
         super().__init__(CompanionCube.cc, position)
-        self.scale = .25
+        self.scale = 1/8
 class PortalGame(App):
     def __init__(self, width, height):
         super().__init__(width, height)
