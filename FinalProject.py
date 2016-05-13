396db44f3d703386d74be9e11aeb64e3ad06f782
averywallis <averybwallis@gmail.com>
2016-05-13 14:24:33 -0400

    Updated from Brython Server: 5/13/2016 2:24:33 PM

--- FinalProject.py
+++ FinalProject.py
@@ -292,6 +292,8 @@ class OpenDoor(Sprite):
     def step(self):
         global win
         self.win = win
+        if self.win == -1:
+            self.x = 0
 class WinDoor(Sprite):
     exitsmall=CircleAsset(20, thinline, platc)
     def __init__(self, position):
