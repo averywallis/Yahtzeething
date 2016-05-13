f82f863692d4a20d5ccb4b6789a0add5e81f6e80
averywallis <averybwallis@gmail.com>
2016-05-13 14:30:56 -0400

    Updated from Brython Server: 5/13/2016 2:30:56 PM

--- FinalProject.py
+++ FinalProject.py
@@ -293,8 +293,8 @@ class OpenDoor(Sprite):
         global win
         self.win = win
         if self.win == 1:
-            self.getSpritesbyClass(OrangePortal)
-            OrangePortal.destroy()
+            x =self.getSpritesbyClass(OrangePortal)
+            x.destroy()
 
 class WinDoor(Sprite):
     exitsmall=CircleAsset(20, thinline, platc)
