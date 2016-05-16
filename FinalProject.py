696cea0f2a361479510ab239f1c3b8857af51526
averywallis <averybwallis@gmail.com>
2016-05-16 14:26:25 -0400

    Updated from Brython Server: 5/16/2016 2:26:25 PM

--- FinalProject.py
+++ FinalProject.py
@@ -288,7 +288,7 @@ class Platforms(Sprite):
         super().__init__(Platforms.plat, position)
 
 class ExitDoor(Sprite):
-    door1 = ImageAsset("images/PortalDoor%20thing.jpg", Frame(0,0,600,452), 1, 'vertical')
+    door1 = ImageAsset("images/PortalDoorthing.png", Frame(0,0,600,452), 1, 'vertical')
     def __init__(self, position):
         super().__init__(ExitDoor.door1, position)
 
