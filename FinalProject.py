fbf6cc35c4667dc1ea49d8dbe7b72e24446cb506
averywallis <averybwallis@gmail.com>
2016-05-16 14:40:57 -0400

    Updated from Brython Server: 5/16/2016 2:40:56 PM

--- FinalProject.py
+++ FinalProject.py
@@ -155,11 +155,9 @@ class Chell(Sprite):
         # win
         global win
         self.wining = win
-        """
         if self.collidingWithSprites(ExitDoor) and self.win == 0 and self.wining == 1:
             self.wintxt = YouWin((400,400))
             self.win = 1
-        """
         
         # move right and left
         if self.mright == 1:
