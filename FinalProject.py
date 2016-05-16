9083c1d4558a445e1b5663a0f2063e62b02dd2b3
averywallis <averybwallis@gmail.com>
2016-05-16 14:15:52 -0400

    Updated from Brython Server: 5/16/2016 2:15:52 PM

--- FinalProject.py
+++ FinalProject.py
@@ -291,6 +291,8 @@ class Door(Sprite):
     door = ImageAsset("images/PortalDoor%20thing.jpg", Frame(0,0,100,100),2, 'vertical')
     def __init__(self, position):
         super().__init__(Door.door, position)
+        self.x = 20
+        self.y = 20
         
 class YouWin(Sprite):
     youwintxt = TextAsset(text="YOU WIN!!", width=200, align='center',style='100px Arial', fill=Color(0xff2222,1))
