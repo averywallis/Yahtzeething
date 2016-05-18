0fef3a40a1512482d46eeae62d40eff7cd62920a
averywallis <averybwallis@gmail.com>
2016-05-18 19:56:51 -0400

    Updated from Brython Server: 5/18/2016 7:56:51 PM

--- FinalProject.py
+++ FinalProject.py
@@ -386,7 +386,7 @@ class Glados(Sprite):
             self.p = 1
             self.t = time.time()
             print(time.time())
-        if time.time() > self.t +2:
+        if time.time() > self.t +2 and self.p == 1:
             e2.play()
         
         
