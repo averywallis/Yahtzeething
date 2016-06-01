f11280ebc4ab17efe51fe033906442128401fa60
averywallis <averybwallis@gmail.com>
2016-06-01 13:34:46 -0400

    Updated from Brython Server: 6/1/2016 1:34:45 PM

--- soundtest.py
+++ soundtest.py
@@ -2,4 +2,5 @@ from ggame import Sound, SoundAsset
 
 e5 = Sound(SoundAsset("sounds/00_part1_entry-5.wav"))
 e6 = Sound(SoundAsset("sounds/00_part1_entry-6.wav"))
-e6.play()
+e7 = Sound(SoundAsset("sounds/00_part1_entry-7.wav"))
+e7.play()
