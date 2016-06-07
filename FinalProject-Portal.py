bf73629d992b433e2ac76fd7b4925019cd4e2e4a
averywallis <averybwallis@gmail.com>
2016-06-07 16:14:23 -0400

    Updated from Brython Server: 6/7/2016 4:14:25 PM

--- FinalProject-Portal.py
+++ FinalProject-Portal.py
@@ -507,7 +507,7 @@ class Glados(Sprite):
             e7.play()
             self.p = 7
             self.t = time.time()
-        if time.time() > self.t + 5 and time.time() < self.t +6 and self.p == 7:
+        if time.time() > self.t + 6 and time.time() < self.t + 6.5 and self.p == 7:
             global cox
             global coy
             cox = 100
